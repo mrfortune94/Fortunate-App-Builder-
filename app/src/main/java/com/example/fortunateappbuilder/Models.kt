ce45da1ce227b19ad24f578ac714cbdc74fb872b
mrfortune94 <mrfortune94@gmail.com>
2026-02-14 14:52:37 +1000

    Create Models.kt

---
+++ app/src/main/java/com/example/fortunateappbuilder/Models.kt
@@ -0,0 +1,5 @@
+package com.example.fortunateappbuilder
+
+// This file is optional.
+// All data classes (Message, FlutterProject, WebWrapperProject) are already defined in the other files.
+// You can leave this file empty or delete it.
